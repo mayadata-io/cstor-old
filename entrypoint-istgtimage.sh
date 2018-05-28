3068ee58a66f5aefc4a5213fd02824631bbb391c
moteesh <moteesh@gmail.com>
2018-05-28 13:57:15 +0530

    Revert "init script for initialising and starting istgt properly"
    This reverts commit c4080322fba5613e24ca3ae3f3119d60a24edfca.
    
    Signed-off-by: moteesh <moteesh@gmail.com>

--- entrypoint-istgtimage.sh
+++ entrypoint-istgtimage.sh
@@ -10,7 +10,7 @@ echo  "exit code:" $?
 echo "reference: "  $0 
 }
 
-exec /usr/local/bin/init.sh volname=vol1 portal=127.0.0.1 path=/tmp/cstor size=10g externalIP=127.0.0.1 replication_factor=3 consistency_factor=2 &
+exec /usr/local/bin/istgt &
 
 child=$!
 wait
