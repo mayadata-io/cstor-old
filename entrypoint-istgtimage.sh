1db57ee41f9c5d7ecfd8c9118fa06a7c582aaa57
moteesh <moteesh@gmail.com>
2018-05-29 19:42:14 +0530

    entrypoint script starting istgt with default configuration
    Signed-off-by: moteesh <moteesh@gmail.com>

--- entrypoint-istgtimage.sh
+++ entrypoint-istgtimage.sh
@@ -10,6 +10,21 @@ echo  "exit code:" $?
 echo "reference: "  $0 
 }
 
+volname=vol1
+path=/tmp/cstor
+size=1G
+mkdir -p $path
+touch $volname
+truncate -s $size $path/$volname
+touch $path/sdisk.img
+truncate -s $size $path/sdisk.img
+touch /tmp/ztest1.0a
+truncate -s 1G /tmp/ztest1.0a
+cp /usr/local/etc/bkpistgt/istgt.conf /usr/local/etc/istgt/
+touch /usr/local/etc/istgt/auth.conf
+touch /usr/local/etc/istgt/logfile
+externalIP=127.0.0.1
+export externalIP=$externalIP
 exec /usr/local/bin/istgt &
 
 child=$!
