a0d1bb6c138e7c2f3153edb38f12fe8e769001a4
moteesh <moteesh@gmail.com>
2018-07-24 12:13:42 +0530

    added sed replace also into if condition
    Signed-off-by: moteesh <moteesh@gmail.com>

--- entrypoint-istgtimage.sh
+++ entrypoint-istgtimage.sh
@@ -12,13 +12,13 @@ echo "reference: "  $0
 
 if [ ! -f "/usr/local/etc/istgt/istgt.conf" ];then
 	cp /usr/local/etc/bkpistgt/istgt.conf /usr/local/etc/istgt/
+	sed -i -n '/LogicalUnit section/,$!p' /usr/local/etc/istgt/istgt.conf
 fi
 cp /usr/local/etc/bkpistgt/istgtcontrol.conf /usr/local/etc/istgt/
 touch /usr/local/etc/istgt/auth.conf
 touch /usr/local/etc/istgt/logfile
 export externalIP=0.0.0.0
 service rsyslog start
-sed -i -n '/LogicalUnit section/,$!p' /usr/local/etc/istgt/istgt.conf
 
 exec /usr/local/bin/istgt &
 
